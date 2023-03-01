a0e292404f72b8cd4df0e2f583c3b8e7cb6546b2
Omar Doya <doya.omar@outlook.com>
2023-03-01 23:03:48 +0100

    add EventBusSubscriptionManager methods

---
+++ EventBus/Abstractions/IEventBusSubscriptionManager.cs
@@ -0,0 +1,16 @@
+﻿namespace EventBus.Abstractions;
+
+public interface IEventBusSubscriptionManager
+{
+    void AddSubscription<T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>;
+
+    IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
+
+    IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
+
+    bool HasSubscriptionForEvent(string eventName);
+
+    bool HasSubscriptionForEvent<T>() where T : IntegrationEvent;
+}
--- EventBus/EventBusSubscriptionManager.cs
+++ EventBus/EventBusSubscriptionManager.cs
@@ -1,9 +1,9 @@
 ﻿namespace EventBus;
 
-public class EventBusSubscriptionManager
+public class EventBusSubscriptionManager : IEventBusSubscriptionManager
 {
-    private readonly Dictionary<string, List<SubscriptionInfo>> _handlers;
     private readonly List<Type> _eventTypes;
+    private readonly Dictionary<string, List<SubscriptionInfo>> _handlers;
 
     public EventBusSubscriptionManager()
     {
@@ -11,13 +11,46 @@ public class EventBusSubscriptionManager
         _eventTypes = new List<Type>();
     }
 
+    public static string GetEventName<T>() where T : IntegrationEvent => typeof(T).Name;
+
     public void AddSubscription<T, TH>()
         where T : IntegrationEvent
         where TH : IIntegrationEventHandler<T>
     {
-        var eventName = GetEventName<T>();
+        string eventName = GetEventName<T>();
+        Type eventType = typeof(T);
+        Type handlerType = typeof(TH);
+
+        if (!HasSubscriptionForEvent(eventName))
+        {
+            _handlers.Add(eventName, new List<SubscriptionInfo>());
+        }
+
+        if (!_eventTypes.Contains(eventType))
+        {
+            _eventTypes.Add(eventType);
+        }
+
+        if (_handlers[eventName].Any(si => si.HandlerType == handlerType))
+        {
+            // TODO : create custom exception
+            throw new ArgumentException($"Handler Type {handlerType.Name} already exists for '{eventName}'", handlerType.Name);
+        }
+
+        _handlers[eventName].Add(new SubscriptionInfo(handlerType));
     }
 
-    public string GetEventName<T>()
-        where T : IntegrationEvent => typeof(T).Name;
+    public IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent
+    {
+        return GetHandlersForEvent(eventName: GetEventName<T>());
+    }
+
+    public IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName) => _handlers[eventName];
+
+    public bool HasSubscriptionForEvent<T>() where T : IntegrationEvent
+    {
+        return HasSubscriptionForEvent(eventName: GetEventName<T>());
+    }
+
+    public bool HasSubscriptionForEvent(string eventName) => _handlers.ContainsKey(eventName);
 }
